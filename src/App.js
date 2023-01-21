681b338a81bc6b4e59e7ba92571e9fc5b30b0895
Alysha Reinard <alysha.reinard@gmail.com>
2023-01-21 16:41:44 +0100

    fixed issue with phone numbers not converting

--- src/App.js
+++ src/App.js
@@ -64,14 +64,19 @@ const clean_status = (status) => {
 };
 
 const clean_phone = (number) => {
-
+  console.log(number)
   let new_number = number.replace(/[^0-9 ]/g, "")
+  new_number = new_number.replace(/\s/g, "")
+  console.log(new_number)
   if (new_number.length === 10){
     new_number = "+1-"+new_number.substring(0,3) + '-' + new_number.substring(3,6) + '-' + new_number.substring(6,)
   } else if((new_number.length === 11) & (new_number[0]==='1')){
-    new_number = new_number.substring(1,4) + '-' + new_number.substring(4,7) + '-' + new_number.substring(7,)
+    new_number = "+1-"+new_number.substring(1,4) + '-' + new_number.substring(4,7) + '-' + new_number.substring(7,)
+  } else {
+    console.log("no match")
+    console.log(number)
   }
-
+  console.log(new_number)
     return new_number
   };
 
